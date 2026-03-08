badc5cf595f8bfa491544f817f7d8ff1981e470a
rigopz <224382864+rigopz@users.noreply.github.com>
2026-03-08 13:06:58 +0530

    Update axios baseURL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://job-tracker-omega-ten.vercel.app/api',
 });
 
 export default instance;
